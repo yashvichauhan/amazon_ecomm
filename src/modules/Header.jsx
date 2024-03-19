e9b1658ace45550cecabb8f58705c2f099c03570
yashvichauhan <31180761+yashvichauhan@users.noreply.github.com>
2024-03-18 20:19:59 -0400

    updating header

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header(){
     return(
         <>
-        <h3>Header from Amazon E-commerce application</h3>
+        <h3>Ecommerce Application Amazon Header</h3>
         </>
     )
 }
